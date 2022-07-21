c1e0e16344f82cef5f24cbb574b9fcb00e488baa
konrad <konradnrog@gmail.com>
2022-07-21 22:16:49 +0200

    start create change tametable via button. Code have to clean

--- app/src/main/java/com/example/krakowautobusy/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/krakowautobusy/ui/details/DetailsFragment.kt
@@ -2,6 +2,8 @@ package com.example.krakowautobusy.ui.details
 
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -18,10 +20,16 @@ import com.example.krakowautobusy.ui.map.vehicledata.StatusData
 import com.example.krakowautobusy.ui.map.vehicledata.StopData
 import com.example.krakowautobusy.ui.map.vehicledata.TimeTableData
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import retrofit2.Response
 
 
 class DetailsFragment : Fragment() {
+
+
+    data class IdVehicle(val tripId:String,val vehicleId:String)
 
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
@@ -32,6 +40,13 @@ class DetailsFragment : Fragment() {
     private var lastVehicleStopName=""
     private val LINE_NUMBER_BUNDLE_NAME="lineNumber"
     private var adapterListTimeTable:AdapterTimeTableListView?=null
+    private lateinit var refreshTimeTableRuna:Runnable
+    private lateinit var refreshListVehicleRunnable: Runnable
+    val mainHandler = Handler(Looper.getMainLooper())
+    var vehicles:ArrayList<IdVehicle> = ArrayList<IdVehicle>()
+    var choiceIndex:Int=0
+    //var actualBus:ArrayList<StatusData>=ArrayList<StatusData>()
+
     companion object{
         var numberLine:Int = 0
     }
@@ -52,7 +67,144 @@ class DetailsFragment : Fragment() {
     }
 
 
-    fun showTimeTableLine(nameLine:String,direction:String){
+
+
+    fun updateList(newList:ArrayList<IdVehicle>) = runBlocking {
+        withContext(Dispatchers.Default) {
+            vehicles=newList;
+        }
+    }
+
+    var tripID:String="";
+    var vehicleId:String="";
+    fun refreshTimeTableBlock()= runBlocking {
+        withContext(Dispatchers.Default) {
+            if(vehicles.size>0) {
+                if(choiceIndex>vehicles.size-1){
+                    choiceIndex=0;
+                }
+                tripID = vehicles[choiceIndex].tripId
+                vehicleId = vehicles[choiceIndex].vehicleId
+            }
+        }
+    }
+
+    fun changeIndex()= runBlocking {
+        withContext(Dispatchers.Default) {
+            choiceIndex+=1;
+
+                if(choiceIndex>vehicles.size-1){
+                    choiceIndex=0;
+                }
+
+
+        }
+    }
+
+
+    fun refreshListVehicle(nameLine:String,direction:String){
+        refreshListVehicleRunnable = object : Runnable {
+            override fun run() {
+
+                var zm=6L;
+
+                var listVeh:ArrayList<IdVehicle> = ArrayList<IdVehicle>()
+                Api.getApi().getBusPosition(zm,
+                    fun(response: Response<AllVehicles>) {
+                        if (response.isSuccessful && response.body() != null ) {
+
+                            val allBus = response.body()!!
+                            zm = allBus.lastUpdate
+                            Log.e("ole","KURWA2")
+                            for(x in allBus.vehicles){
+                                if(x.name.contains(nameLine.trim()+" "+direction.trim())) {
+                                    listVeh.add(IdVehicle(x.tripId, x.id))
+
+                                }
+                            }
+                            var listVehs = listVeh.sortedWith(compareBy({ it.tripId.toLong() }))
+
+                            var listVeh2:ArrayList<IdVehicle> = ArrayList<IdVehicle>()
+                            for(x in allBus.vehicles){
+                                if(x.name.contains(nameLine.trim()) and (!x.name.contains(direction.trim()))) {
+                                    listVeh2.add(IdVehicle(x.tripId, x.id))
+                                }
+                            }
+                            var listVehs2=listVeh2.sortedWith(compareBy({ it.tripId.toLong() }))
+
+                            listVeh.clear()
+                            listVeh.addAll(listVehs)
+                            listVeh.addAll(listVehs2)
+
+                        }
+                    }
+                )
+               updateList(listVeh)
+
+
+
+                mainHandler.postDelayed(this, 60000)
+            }
+
+
+            // mainHandler.postDelayed(this,5000L)
+            // mainHandler;
+        }
+
+        mainHandler.post(refreshListVehicleRunnable)
+    }
+
+
+
+    fun refreshTimeTableBaseList(){
+        refreshTimeTableBlock()
+        Api.getApi().getTimeTableVehicle(tripID,vehicleId, fun(response: Response<TimeTableData>)  {
+            Log.e("ole",response.raw().request().url().toString())
+            //Log.e("ole",response.raw().request(). toString())
+            Log.e("ole",response.raw().request().headers() .toString())
+            Log.e("ole",response.errorBody().toString())
+            Log.e("ole",response.message(). toString())
+            Log.e("ole",response.headers().toString()  )
+            if (response.isSuccessful) {
+                val ac = response.body()!!
+                //  ac.actual.addAll(ac.old)
+                Log.e("oleK",ac.actual.size .toString())
+                for(x in ac.actual){
+                    Log.e("oleK",x.actualTime)
+                }
+                ac.old.addAll(ac.actual)
+                adapterListTimeTable?.changeDataset(ac.old)
+
+
+                //   Log.e("ole",ac.old[0].name+" KUR")
+            }
+        })
+    }
+
+
+
+
+    fun refreshTimeTable(){
+        refreshTimeTableRuna = object : Runnable {
+            override fun run() {
+
+
+                refreshTimeTableBaseList()
+
+
+
+                mainHandler.postDelayed(this, 10000)
+                }
+
+
+               // mainHandler.postDelayed(this,5000L)
+               // mainHandler;
+            }
+
+        mainHandler.post(refreshTimeTableRuna)
+    }
+
+ /*   fun showTimeTableLine(nameLine:String,direction:String){
         var zm:Long=0
         Log.e("ole","KURWA")
         Api.getApi().getBusPosition(zm,
@@ -114,7 +266,7 @@ class DetailsFragment : Fragment() {
                 // showAllVehicle(map, allTram)
             }
         })
-    }
+    }*/
 
     fun messageForMapFragment(numberLine:Int){
         val result = Bundle()
@@ -133,7 +285,11 @@ class DetailsFragment : Fragment() {
         adapterListTimeTable=AdapterTimeTableListView(ArrayList<StatusData>(),requireContext())
         binding.listTimeTable.adapter=adapterListTimeTable
         Log.e("ole", numberLine.toString()+" "+lastVehicleStopName)
-        showTimeTableLine(numberLine.toString(),lastVehicleStopName)
+     //   showTimeTableLine(numberLine.toString(),lastVehicleStopName)
+       // refreshTimeTable(numberLine.toString(),lastVehicleStopName)
+
+        refreshListVehicle(numberLine.toString(),lastVehicleStopName)
+        refreshTimeTable()
     }
 
 
@@ -151,6 +307,10 @@ class DetailsFragment : Fragment() {
         DetailsFragment.numberLine=lineNumber
         binding.details.currentlyFollowingFirstBusStopData.text="$firstVehicleStopName"
         binding.details.currentlyFollowingLastBusStopData.text="$lastVehicleStopName"
+
+        binding.details.changeDirectionButton.setOnClickListener {
+               changeIndex()
+        }
 
 
     }
